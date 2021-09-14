3a428c6a5b0323df8f1595e4163c23c518e3c3dc
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2021-09-13 21:39:20 -0500

    feature/initial-page

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React  from 'react';
+import imgprimary from './img/149331902_258445488988259_9048814731489784611_n.jpg'
+import './App.css';
+import Navbar from './component/Navbar/Navbar';
+import Bibliography from './component/Bibliography/Bibliography'
+import Galery from './component/Galery/Galery';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img 
+          src={imgprimary} 
+          className="App-logo" 
+          alt="logo" 
+        />
+          <Navbar className={"navbar"}/>
+        <Bibliography/>
+        <Galery/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Galery/Galery.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Galery = () => {
+    return (
+        <>
+            {/* panel izquierdo para la galeria de imagenes */}
+            <div className={"panelleft"}>
+                
+            </div>
+            {/* panel derecho para los eventos  */}
+            <div className={"panelrght"}>
+
+            </div>
+        </>
+    )
+}
+
+export default Galery
